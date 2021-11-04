6bd649bc855ddae91697e60a9a643639c54625ba
skairos <stan.durand@skairos.io>
2021-11-04 11:16:55 +0100

    last version with glfw

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 
 #include"add/add.h"
 
+#include"external/glfw/include/GLFW/glfw3.h"
 
 using namespace std;
 
@@ -10,6 +11,49 @@ int main()
 	cout << "hello" << endl;
 
 	cout << add(3.2, 4.2) << endl;
+
+
+	GLFWwindow *window;
+
+	if( !glfwInit() )
+	{
+	  fprintf( stderr, "Failed to initialize GLFW\n" );
+	  exit( EXIT_FAILURE );
+	}
+
+//	glfwWindowHint(GLFW_DEPTH_BITS, 16);
+//	glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+
+
+  	window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+
+	if (!window)
+	{
+	  fprintf( stderr, "Failed to open GLFW window\n" );
+	  glfwTerminate();
+	  exit( EXIT_FAILURE );
+	}
+
+
+
+
+	// Main loop
+	while( !glfwWindowShouldClose(window) )
+	{
+		// Swap buffers
+	  	glfwSwapBuffers(window);
+	  	glfwPollEvents();
+	}
+
+	// Terminate GLFW
+	glfwTerminate();
+
+	// Exit program
+	exit( EXIT_SUCCESS );
+
+
+
 }
 
 
